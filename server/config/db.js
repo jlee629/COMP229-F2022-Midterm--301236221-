285bd3efd1045ae0669dc5bef8bbf7994f1e7ec7
Jungyu Lee <jlee629@my.centennialcollege.ca>
2022-10-24 11:41:22 -0400

    mongo atlas added

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //URI: "mongodb://127.0.0.1/faculty_info",
+  URI: "mongodb+srv://won:lfhHZrBy1Ik8guZj@mongodbserver.yfom0su.mongodb.net/faculty_info?retryWrites=true&w=majority"
+};
