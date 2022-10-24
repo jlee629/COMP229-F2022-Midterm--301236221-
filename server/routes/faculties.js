486861d7684b19c1b8bbb8d46b7fb132d27c2a06
Jungyu Lee <jlee629@my.centennialcollege.ca>
2022-10-24 12:57:16 -0400

    routers added

--- server/routes/faculties.js
+++ server/routes/faculties.js
@@ -23,34 +23,78 @@ router.get("/", (req, res, next) => {
 });
 
 //  GET the faculty Details page in order to add a new faculty
-router.get("/add", (req, res, next) => {});
+router.get("/add", (req, res, next) => {
+  res.render("faculties/add", { title: "Add Faculty" });
+});
 
 // POST process the faculty  Details page and create a new faculty  - CREATE
 router.post("/add", (req, res, next) => {
-  /*****************
-   * ADD CODE HERE *
-   *****************/
+  let newfaculty = faculty({
+    Facultyid: req.body.Facultyid,
+    Facultyname: req.body.Facultyname,
+    Department: req.body.Department,
+    Subject: req.body.Subject,
+  });
+  faculty.create(newfaculty, (err, faculty) => {
+    if (err) {
+      console.log(err);
+      res.end(err);
+    } else {
+      //refresh the faculty-list
+      res.redirect("faculties/index");
+    }
+  });
 });
 
 // GET the faculty  Details page in order to edit an existing faculty
-router.get("/:id", (req, res, next) => {
-  /*****************
-   * ADD CODE HERE *
-   *****************/
+router.get("details/:id", (req, res, next) => {
+  let id = req.params.id; //id of actual object
+
+  faculty.findById(id, (err, facultytoedit) => {
+    if (err) {
+      console.log(err);
+      res.end(err);
+    } else {
+      //show the edit view
+      res.render("faculties/details", { title: "Edit Faculty", faculty: facultytoedit });
+    }
+  });
 });
 
 // POST - process the information passed from the details form and update the document
 router.post("/:id", (req, res, next) => {
-  /*****************
-   * ADD CODE HERE *
-   *****************/
+  let id = req.params.id; //id of actual object
+
+  let updatefaculty = faculty({
+    _id: id,
+    Facultyid: req.body.Facultyid,
+    Facultyname: req.body.Facultyname,
+    Department: req.body.Department,
+    Subject: req.body.Subject,
+  });
+  faculty.updateOne({ _id: id }, updatefaculty, (err) => {
+    if (err) {
+      console.log(err);
+      res.end(err);
+    } else {
+      //refresh the book list
+      res.redirect("faculties/index");
+    }
+  });
 });
 
 // GET - process the delete
 router.get("/delete", (req, res, next) => {
-  /*****************
-   * ADD CODE HERE *
-   *****************/
+  let id = req.params.id;
+  faculty.remove({ _id: id }, (err) => {
+    if (err) {
+      console.log(err);
+      res.end(err);
+    } else {
+      //refresh book list
+      res.redirect("faculties/index");
+    }
+  });
 });
 
 module.exports = router;
